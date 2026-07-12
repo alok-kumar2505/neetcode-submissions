dae70628a70512be6a425c7efa321c4b325cab69
Alok Kumar <148738056+alok-kumar2505@users.noreply.github.com>
2026-07-12 13:49:05 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        if(n==0) return 0;
+        HashSet <Integer> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(nums[i]);
+        }
+        int longest=1;
+
+        for(int val:set){
+        if(!set.contains(val-1)){
+            int cnt=1;
+            int x=val;
+            while(set.contains(x+1)){
+                cnt=cnt+1;
+                x=x+1;
+            }
+            if(cnt>longest) longest=cnt;
+
+        }
+                }
+        return longest;
+    }
+}
